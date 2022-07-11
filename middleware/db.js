c28e6f73c447dc022c186feef5647fa831317fec
Vraj-11 <vrajroy2k19@gmail.com>
2022-07-11 16:51:39 +0530

    winnk

--- middleware/db.js
+++ middleware/db.js
@@ -5,8 +5,8 @@ const  connectDb = async()=>{
     if(mongoose.connections[0].readyState){  
         return;
     }
-       mongoose.connect("mongodb://localhost:27017/hirelancer") .then(() => console.log("Database connected!"))
-    //    mongoose.connect("mongodb+srv://Vraj:yiZyCa8ipDI8kqWW@hirelancercluster.xgdl5ud.mongodb.net/hirelancer") .then(() => console.log("Database connected!"))
+    //    mongoose.connect("mongodb://localhost:27017/hirelancer") .then(() => console.log("Database connected!"))
+       mongoose.connect("mongodb+srv://Vraj:yiZyCa8ipDI8kqWW@hirelancercluster.xgdl5ud.mongodb.net/hirelancer") .then(() => console.log("Database connected!"))
        .catch(err => console.log(err));
         
 
