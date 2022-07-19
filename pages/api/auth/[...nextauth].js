1144419bc986b072ea78a4c11bf929567ccc1318
Vraj-11 <vrajroy2k19@gmail.com>
2022-07-19 17:10:24 +0530

    19-7

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,8 +5,10 @@ import clientPromise from './mongo'
 export default NextAuth({
     providers:[
         GoogleProvider({
-             clientId:process.env.CLIENT_ID,
-             clientSecret:process.env.CLIENT_SECRET,
+            //  clientId:"770196948820-9t6prrdkjmunndedktub7m3m1qocgl36.apps.googleusercontent.com",
+            //  clientSecret:"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",
+             clientId:"770196948820-ih0tr4cpkhcj2dbi0bu9hbbq02tj58f0.apps.googleusercontent.com",
+             clientSecret:"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",
              secret:"v",
             }),   
     ], 
